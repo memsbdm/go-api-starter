c5dd0b245a54edce7552723d914b8a2ed86dd4ac
memsbdm <mehmetbdm@outlook.fr>
2025-01-15 11:21:02 +0100

    test: refresh token

--- internal/domain/services/services_test/auth_service_test.go
+++ internal/domain/services/services_test/auth_service_test.go
@@ -5,9 +5,11 @@ package services_test
 import (
 	"context"
 	"errors"
+	"go-starter/internal/adapters/timegen"
 	"go-starter/internal/domain"
 	"go-starter/internal/domain/entities"
 	"testing"
+	"time"
 )
 
 func TestAuthService_Login(t *testing.T) {
@@ -60,6 +62,61 @@ func TestAuthService_Login(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			_, _, err := builder.AuthService.Login(ctx, tt.input.username, tt.input.password)
+			if !errors.Is(err, tt.expectedErr) {
+				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestAuthService_Refresh(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		advance     time.Duration
+		expectedErr error
+	}{
+		{
+			name:        "Refresh with a valid refresh token",
+			advance:     0,
+			expectedErr: nil,
+		},
+		{
+			name:        "Refresh with an expired refresh token",
+			advance:     refreshTokenExpirationDuration,
+			expectedErr: domain.ErrInvalidToken,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Arrange
+			ctx := context.Background()
+			timeGenerator := timegen.NewFakeTimeGenerator(time.Now())
+			builder := NewTestBuilder().WithTimeGenerator(timeGenerator).Build()
+
+			userToCreate := &entities.User{
+				Username: "example",
+				Password: "secret123",
+			}
+
+			_, err := builder.AuthService.Register(ctx, userToCreate)
+			if err != nil {
+				t.Fatalf("failed to register user: %v", err)
+			}
+
+			_, refreshToken, err := builder.AuthService.Login(ctx, userToCreate.Username, userToCreate.Password)
+			if err != nil {
+				t.Fatalf("failed to login: %v", err)
+			}
+
+			builder.TimeGenerator.Advance(tt.advance)
+
+			// Act & Assert
+			_, _, err = builder.AuthService.Refresh(ctx, refreshToken)
 			if !errors.Is(err, tt.expectedErr) {
 				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
 			}
--- internal/domain/services/services_test/token_service_test.go
+++ internal/domain/services/services_test/token_service_test.go
@@ -3,6 +3,7 @@
 package services_test
 
 import (
+	"context"
 	"errors"
 	"github.com/google/uuid"
 	"go-starter/internal/adapters/timegen"
@@ -55,7 +56,106 @@ func TestTokenService_ValidateAndParseAccessToken(t *testing.T) {
 			builder.TimeGenerator.Advance(tt.advance)
 
 			// Act & Assert
-			_, err = builder.TokenService.ValidateAndParseAccessToken(accessToken)
+			claims, err := builder.TokenService.ValidateAndParseAccessToken(accessToken)
+			if !errors.Is(err, tt.expectedErr) {
+				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+			}
+
+			if err == nil && claims.Subject != user.ID {
+				t.Errorf("expected user id %s, got %s", user.ID, claims.Subject)
+			}
+		})
+	}
+}
+
+func TestTokenService_ValidateAndParseRefreshToken(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		advance     time.Duration
+		expectedErr error
+	}{
+		{
+			name:        "Validate and parse valid refresh token",
+			advance:     0,
+			expectedErr: nil,
+		},
+		{
+			name:        "Validate and parse expired refresh token",
+			advance:     refreshTokenExpirationDuration,
+			expectedErr: domain.ErrInvalidToken,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Arrange
+			ctx := context.Background()
+			timeGenerator := timegen.NewFakeTimeGenerator(time.Now())
+			builder := NewTestBuilder().WithTimeGenerator(timeGenerator).Build()
+			user := &entities.User{
+				ID: entities.UserID(uuid.New()),
+			}
+			refreshToken, err := builder.TokenService.GenerateRefreshToken(ctx, user.ID)
+			if err != nil {
+				t.Fatalf("failed to generated refresh token: %v", err)
+			}
+			builder.TimeGenerator.Advance(tt.advance)
+
+			// Act & Assert
+			claims, err := builder.TokenService.ValidateAndParseRefreshToken(ctx, refreshToken)
+			if !errors.Is(err, tt.expectedErr) {
+				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+			}
+			if err == nil && claims.Subject != user.ID {
+				t.Errorf("expected user id %s, got %s", user.ID, claims.Subject)
+			}
+		})
+	}
+}
+
+func TestTokenService_RevokeRefreshToken(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		advance     time.Duration
+		expectedErr error
+	}{
+		{
+			name:        "Revoke valid refresh token",
+			advance:     0,
+			expectedErr: nil,
+		},
+		{
+			name:        "Revoke expired refresh token",
+			advance:     accessTokenExpirationDuration,
+			expectedErr: domain.ErrInvalidToken,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Arrange
+			ctx := context.Background()
+			timeGenerator := timegen.NewFakeTimeGenerator(time.Now())
+			builder := NewTestBuilder().WithTimeGenerator(timeGenerator).Build()
+			user := &entities.User{
+				ID: entities.UserID(uuid.New()),
+			}
+
+			refreshToken, err := builder.TokenService.GenerateRefreshToken(ctx, user.ID)
+			if err != nil {
+				t.Fatalf("failed to generated refresh token: %v", err)
+			}
+			builder.TimeGenerator.Advance(tt.advance)
+
+			err = builder.TokenService.RevokeRefreshToken(ctx, refreshToken)
 			if !errors.Is(err, tt.expectedErr) {
 				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
 			}
